c674ba010fd06b5f8d065db2a3d9625a42f1b729
polandll <lorinapoland@gmail.com>
2021-01-13 17:27:36 -0800

    change docker compose files

--- cass4-sg-zepp/start_cass-sg-zepp.sh
+++ cass4-sg-zepp/start_cass-sg-zepp.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+export CASSTAG=3.11.8
+export SGTAG=v1.0.2
+export ZEPPTAG=0.9.0
+
 # Make sure backend-1, the seed node, is up before bringing up other nodes and stargate
 docker-compose up -d backend-1
 # Wait until the seed node is up before bringing up more nodes
@@ -7,10 +11,10 @@ docker-compose up -d backend-1
 
 # Bring up the 2nd C* node
 #docker-compose up -f $COMPOSE_FILE -d backend-2
-#(docker-compose logs -f backend-1 &) | grep -q "is now part of the cluster"
+#(docker-compose logs -f backend-2 &) | grep -q "is now part of the cluster"
 # Bring up the 3rd C* node
 #docker-compose up -f $COMPOSE_FILE -d backend-3
-#(docker-compose logs -f backend-1 &) | grep -q "is now part of the cluster"
+#(docker-compose logs -f backend-3 &) | grep -q "is now part of the cluster"
 
 # Bring up the stargate
 docker-compose up -d stargate
