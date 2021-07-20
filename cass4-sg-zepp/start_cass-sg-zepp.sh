124bf313e4354ecf93f450f5ff29bcf5ddecbc92
Lorina Poland <lorinapoland@gmail.com>
2021-07-20 10:57:22 -0700

    change docker-compose to docker compose

--- cass4-sg-zepp/start_cass-sg-zepp.sh
+++ cass4-sg-zepp/start_cass-sg-zepp.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
 export CASSTAG=4.0
-export SGTAG=v1.0.18
+export SGTAG=v1.0.29
 export ZEPPTAG=0.9.0
 
 # Make sure backend-1, the seed node, is up before bringing up other nodes and stargate
-docker-compose up -d backend-1
+docker compose up -d backend-1
 # Wait until the seed node is up before bringing up more nodes
-(docker-compose logs -f backend-1 &) | grep -q "Created default superuser role"
+(docker compose logs -f backend-1 &) | grep -q "Created default superuser role"
 
 # Bring up the 2nd C* node
-#docker-compose up -f $COMPOSE_FILE -d backend-2
-#(docker-compose logs -f backend-2 &) | grep -q "is now part of the cluster"
+#docker compose up -f $COMPOSE_FILE -d backend-2
+#(docker compose logs -f backend-2 &) | grep -q "is now part of the cluster"
 # Bring up the 3rd C* node
-#docker-compose up -f $COMPOSE_FILE -d backend-3
-#(docker-compose logs -f backend-3 &) | grep -q "is now part of the cluster"
+#docker compose up -f $COMPOSE_FILE -d backend-3
+#(docker compose logs -f backend-3 &) | grep -q "is now part of the cluster"
 
 # Bring up the stargate
-docker-compose up -d stargate
+docker compose up -d stargate
 # Wait until stargate is up before bringing up the metrics tools
 echo ""
 echo "Waiting for stargate to start up..."
@@ -27,4 +27,4 @@ while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:8082/healt
 done
 
 # Bring up zeppelin
-docker-compose up -d zeppelin
+docker compose up -d zeppelin
