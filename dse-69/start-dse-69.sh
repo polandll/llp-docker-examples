a03475ac42b8aeeebd00ae00041108d59a6a3e27
Lorina Poland <lorina@datastax.com>
2024-05-23 11:25:59 -0700

    Mod dse-69

--- dse-69/start-dse-69.sh
+++ dse-69/start-dse-69.sh
@@ -5,7 +5,7 @@ read num_nodes
 
 export DSETAG=6.9.0-early-preview-java11
 export std_port=9042
-export fwd_port=9034
+export fwd_port=9035
 export node_name_1='backend-dse69-1'
 export node_name_2='backend-dse69-2'
 export node_name_3='backend-dse69-3'
